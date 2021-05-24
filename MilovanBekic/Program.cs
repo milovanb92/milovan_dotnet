a6f15d955c84012039d44900ab94d43004ecca47
Milovan Bekic <milovanbekic@gmail.com>
2021-05-24 12:25:26 +0000

    Added url

--- MilovanBekic/Program.cs
+++ MilovanBekic/Program.cs
@@ -20,7 +20,7 @@ namespace MilovanBekic
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls(new[] { "http://0.0.0.0:5000" });
                 });
     }
 }
